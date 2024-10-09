5930c0599e76ba185ba9654a1033912d45caa2e3
Caio-Marinho <kaigabriel12@gmail.com>
2024-10-09 11:35:25 -0300

    classe e aplicação

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from classe import Pessoa
 pessoa = Pessoa('caio',65,24)
 pessoa.nome = 'Caio Gabriel Marinho'
 print(pessoa.nome)
-pessoa.comer()
+pessoa.acordar()
 pessoa.terminou()
-pessoa.terminou()
-pessoa.dormir()
+pessoa.parou()
