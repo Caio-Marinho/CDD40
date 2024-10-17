10c9656b36481e4fe47e8af42e1bc5872548040a
Caio Marinho <108732958+Caio-Marinho@users.noreply.github.com>
2024-10-17 10:54:43 -0300

    Add files via upload

--- Tratamento_erro_e_manipulacao/Biblioteca.py
+++ Tratamento_erro_e_manipulacao/Biblioteca.py
@@ -7,3 +7,12 @@ def mostrar():
         leitura = arq2.read()
         yield leitura
 
+def pesquisar(texto):
+    contagem = 0
+    with open('Registro.txt','r',encoding='UTF-8') as Arquivo:
+        for text in Arquivo:
+            if texto in text:
+                contagem+=1
+        return contagem
+
+
--- Tratamento_erro_e_manipulacao/execution.py
+++ Tratamento_erro_e_manipulacao/execution.py
@@ -1,18 +1,24 @@
 from Biblioteca import *
 
 while True:
-    lista = ['Inserir','Mostrar','Sair']
+    lista = ['Inserir','Mostrar','Sair','Pesquisar']
     for indice,item in enumerate(lista):
         print(f"{indice+1}. {item}")
-    opcao = int(input("Digite o codigo da opção: "))
-    if opcao == 1:
-        cadastra(input("Digite um texto: "))
-    elif opcao == 2:
-        leitura = mostrar()
-        for texto in leitura:
-            print(texto)
-    elif opcao == 3:
-        print("Fim de Execução!!")
-        break
-    else:
-        print("Insira um codigo válido")
+    try:
+        opcao = int(input("Digite o codigo da opção: "))
+        if opcao == 1:
+            cadastra(input("Digite um texto: "))
+        elif opcao == 2:
+            leitura = mostrar()
+            for texto in leitura:
+                print(texto)
+        elif opcao == 3:
+            print("Fim de Execução!!")
+            break
+        elif opcao == 4:
+            x = pesquisar(input("Digite o que desenja pesquisar: "))
+            print(x)
+        else:
+            print("Insira um codigo válido")
+    except ValueError:
+        print("Digite um codigo")
